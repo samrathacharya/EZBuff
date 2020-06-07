7786563aad31ba0b690524f61d90ee45cfcca7ae
samrathacharya <z5161678@student.unsw.edu.au>
2020-06-07 12:09:26 +1000

    Made changes

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production'){
     // Load index.html file
     app.get('*', (req,res) =>{
         res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
-    })
+    });
 }
 
 //Connect port
